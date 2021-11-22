5cc9fba6221399006fb345e7c87195a1950ffda2
Kungger-git <matthewriel63@gmail.com>
2021-11-22 17:06:58 +0800

    Update build script

--- core_pkgbuild/zone-grub-theme/build.sh
+++ core_pkgbuild/zone-grub-theme/build.sh
@@ -10,7 +10,6 @@ echo "################################################################"
 echo "################### Moved the .zst file to x86_64 folder"
 echo "################################################################"
 find . -type d -name "pkg" -name "src" -exec sudo rm -r "{}" \;
-find . -type f -not -name "build.sh" -not -name "*.install" -not -name PKGBUILD -not -name "*.pkg.tar.zst" -delete
 echo "################################################################"
 echo "################### Removed all redundant files and folders"
 echo "################################################################"
