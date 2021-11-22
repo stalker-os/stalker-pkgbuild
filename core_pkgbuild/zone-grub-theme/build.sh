31476fec559bbe8c3d580ff301005ed7ffd010e9
Kungger-git <matthewriel63@gmail.com>
2021-11-22 14:29:21 +0800

    Change branding

--- core_pkgbuild/zone-grub-theme/build.sh
+++ core_pkgbuild/zone-grub-theme/build.sh
@@ -9,6 +9,7 @@ mv *.pkg.tar.zst ../../x86_64
 echo "################################################################"
 echo "################### Moved the .zst file to x86_64 folder"
 echo "################################################################"
+find . -type d -name "pkg" -name "src" -exec sudo rm -r "{}" \;
 find . -type f -not -name "build.sh" -not -name "*.install" -not -name PKGBUILD -not -name "*.pkg.tar.zst" -delete
 echo "################################################################"
 echo "################### Removed all redundant files and folders"
