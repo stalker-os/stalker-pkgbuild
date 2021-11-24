95e4b93867bdd1103ac94039578c7635d371517d
Kungger-git <matthewriel63@gmail.com>
2021-11-24 15:31:59 +0800

    Update grub

--- core_pkgbuild/grub/build.sh
+++ core_pkgbuild/grub/build.sh
@@ -10,7 +10,7 @@ echo "################################################################"
 echo "################### Moved the .zst file to x86_64 folder"
 echo "################################################################"
 find . -type d -not -path "." -exec sudo rm -r "{}" \;
-find . -type f -not -name "build.sh" -not -name "*.install" -not -name "*.patch" -not -name "*.hook" -not -name "grub-set-bootflag" -not -name "*.cfg" -not -name "*.default" -not -name "update-grub" -not -name "60_memtest86+" -not -name "background.png" -not -name PKGBUILD -not -name "*.pkg.tar.zst" -delete
+find . -type f -not -name "build.sh" -not -name "*.install" -not -name "*.patch" -not -name "*.hook" -not -name "grub-set-bootflag" -not -name "*.cfg" -not -name "*.default" -not -name "update-grub" -not -name "60_memtest86+" -not -name "background.png" -not -name PKGBUILD -not -name "unifont-13.0.06.bdf.gz" -not -name "*.pkg.tar.zst" -delete
 echo "################################################################"
 echo "################### Removed all redundant files and folders"
 echo "################################################################"
