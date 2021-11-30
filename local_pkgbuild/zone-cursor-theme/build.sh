de5b1b5c05a6823469674f938719fcde1aa67003
Kungger-git <matthewriel63@gmail.com>
2021-11-30 21:19:35 +0800

    Fix build script

--- local_pkgbuild/zone-cursor-theme/build.sh
+++ local_pkgbuild/zone-cursor-theme/build.sh
@@ -9,8 +9,7 @@ mv *.pkg.tar.zst ../../x86_64
 echo "################################################################"
 echo "################### Moved the .zst file to x86_64 folder"
 echo "################################################################"
-find . -type d -not -path "." -not -path "./Zone" -exec sudo rm -r "{}" \;
-find . -type f -not -name "build.sh" -not -name "*.install" -not -name PKGBUILD -not -name "index.theme" -not -name "*.pkg.tar.zst" -delete
+find . -type d -name "pkg" -name "src" -exec sudo rm -r "{}" \;
 echo "################################################################"
 echo "################### Removed all redundant files and folders"
 echo "################################################################"
